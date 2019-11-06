7f8c874e84eec6e55521e3fedabc765d9d6db64c
Alexander A. Klimov <alexander.klimov@icinga.com>
2019-11-06 15:40:38 +0100

    Implement process manager library for C in Go
    refs #7600

---
+++ libgo/procmgr/c.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stddef.h>
+// size_t
+
+#include <stdint.h>
+// uint64_t
+// uintptr_t
+
+#include <sys/types.h>
+// pid_t
+
+typedef unsigned char uchar;
+
+static inline
+void invokeCallback(uintptr_t cb, uintptr_t cbData, uint64_t iPid, pid_t pid, int exitCode, uintptr_t output, size_t outputLen)
+{
+	(*(void(*)(void*, uint64_t, pid_t, int, const char*, size_t))cb)(
+		(void*)cbData, iPid, pid, exitCode, (const char*)output, outputLen
+	);
+}
